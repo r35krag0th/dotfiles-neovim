99503822208b5a770fe73ce2b0c44eaf1532256b
Bob Saska <git@r35.net>
2024-06-10 17:50:50 -0500

    fix: make neorg -> norg

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -2,7 +2,7 @@ return {
   "L3MON4D3/LuaSnip",
   config = function()
     local ls = require("luasnip")
-    local r = require("r35.snippets.neorg_snippets")
+    local r = require("r35.snippets.norg_snippets")
     ls.add_snippets("norg", {
       r.calendar_entry,
       r.free_time_entry,
